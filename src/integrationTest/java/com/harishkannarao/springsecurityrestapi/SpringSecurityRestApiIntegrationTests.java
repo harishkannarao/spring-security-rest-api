4bfef03f5026e7cf9372ee66be96ae6024df995c
harishkannarao <harishkannarao@users.noreply.github.com>
2022-12-29 23:16:09 +0000

    Autowiring in tests

--- src/integrationTest/java/com/harishkannarao/springsecurityrestapi/SpringSecurityRestApiIntegrationTests.java
+++ src/integrationTest/java/com/harishkannarao/springsecurityrestapi/SpringSecurityRestApiIntegrationTests.java
@@ -2,10 +2,13 @@ package com.harishkannarao.springsecurityrestapi;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SpringSecurityRestApiIntegrationTests extends AbstractBaseDefaultProfileIntegrationTest {
 
 	@Test
 	void contextLoads() {
+		assertThat(getTestApplicationUrl()).isNotBlank();
 	}
 
 }
--- src/testFixtures/java/com/harishkannarao/springsecurityrestapi/AbstractBaseIntegrationTest.java
+++ src/testFixtures/java/com/harishkannarao/springsecurityrestapi/AbstractBaseIntegrationTest.java
@@ -1,7 +1,27 @@
 package com.harishkannarao.springsecurityrestapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.util.TestSocketUtils;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+)
 abstract class AbstractBaseIntegrationTest {
+
+    private static final int RANDOM_SERVER_PORT = TestSocketUtils.findAvailableTcpPort();
+
+    @DynamicPropertySource
+    static void registerTestProperties(DynamicPropertyRegistry registry) {
+        registry.add("server.port", () -> String.valueOf(RANDOM_SERVER_PORT));
+    }
+
+    @Value("${test.application.url}")
+    private String testApplicationUrl;
+
+    protected String getTestApplicationUrl() {
+        return testApplicationUrl;
+    }
 }

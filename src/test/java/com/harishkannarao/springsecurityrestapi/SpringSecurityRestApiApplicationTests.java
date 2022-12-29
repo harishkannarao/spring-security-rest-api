c538afc73f8705f246dca19d598dd80f4b311f42
harishkannarao <harishkannarao@users.noreply.github.com>
2022-12-29 19:53:41 +0000

    Initial commit of code generated by spring intializr

---
+++ src/test/java/com/harishkannarao/springsecurityrestapi/SpringSecurityRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harishkannarao.springsecurityrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

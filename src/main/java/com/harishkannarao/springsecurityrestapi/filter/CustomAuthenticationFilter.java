b127c6f6f209c7ecb6fc90729a8d848d956d069e
harishkannarao <harishkannarao@users.noreply.github.com>
2023-01-13 23:38:55 +0000

    Using builder for User

--- src/main/java/com/harishkannarao/springsecurityrestapi/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/harishkannarao/springsecurityrestapi/filter/CustomAuthenticationFilter.java
@@ -7,25 +7,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.util.List;
 
 @Component
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final User user = new User(
-                "username",
-                "",
-                true,
-                true,
-                true,
-                true,
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+
+        final UserDetails user = User.builder()
+                .username("username")
+                .password("")
+                .disabled(false)
+                .accountExpired(false)
+                .credentialsExpired(false)
+                .accountLocked(false)
+                .authorities(List.of(new SimpleGrantedAuthority("ROLE_USER")))
+                .build();
         final UsernamePasswordAuthenticationToken authentication =
                 new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 //        SecurityContextHolder.getContext().setAuthentication(authentication);

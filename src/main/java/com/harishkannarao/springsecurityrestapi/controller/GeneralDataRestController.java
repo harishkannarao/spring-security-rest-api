350fe8a4861f7ca8b761da02ab43bad3615e78c7
harishkannarao <harishkannarao@users.noreply.github.com>
2024-10-27 21:27:33 +0000

    Adding log statement

--- src/main/java/com/harishkannarao/springsecurityrestapi/controller/GeneralDataRestController.java
+++ src/main/java/com/harishkannarao/springsecurityrestapi/controller/GeneralDataRestController.java
@@ -1,6 +1,8 @@
 package com.harishkannarao.springsecurityrestapi.controller;
 
 import com.harishkannarao.springsecurityrestapi.domain.GeneralData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = {"/general-data"})
 public class GeneralDataRestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(GeneralDataRestController.class);
+
     @GetMapping
     public ResponseEntity<GeneralData> getGeneralData() {
+        logger.info("Received request for /general-data");
         return ResponseEntity.accepted().body(GeneralData.builder()
                 .message("Welcome !!!")
                 .build());
